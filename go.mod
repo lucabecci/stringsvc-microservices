120ab059fb17c65b24ed08bf730949b21b4d394b
luca <beccibrian@gmail.com>
2021-03-01 12:16:04 -0300

    chore(@all): Docker multi-stage and actions for build added

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/lucabecci/stringsvc-microservices
 go 1.16
 
 require (
-	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/go-kit/kit v0.10.0
 	github.com/prometheus/client_golang v1.9.0
 )
--- transports/stringService.go
+++ transports/stringService.go
@@ -27,3 +27,6 @@ func (stringService) Uppercase(s string) (string, error) {
 func (stringService) Count(s string) int {
 	return len(s)
 }
+
+// ServiceMiddleware is a chainable behavior modifier for StringService.
+type ServiceMiddleware func(StringService) StringService
